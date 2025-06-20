c18c4b70b2ab92355c2cf527411a32541979830f
Yoonyoul <yoonyoul@gmail.com>
2025-06-20 21:06:22 +0900

    infra-local: redis-cluster

--- containers/redis/run-redis-on-mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export REDIS_CLUSTER_IP=0.0.0.0
-docker-compose up
--- containers/run-infra-apps.sh
+++ containers/run-infra-apps.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export REDIS_CLUSTER_IP=0.0.0.0
 docker-compose  -f ./docker-compose-infra-apps.yaml up -d
